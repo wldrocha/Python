d0e04cf3490460084be817ed9b8dfb1bf4a44a9f
Wladimir Rocha <wld.rocha@gmail.com>
2018-10-21 15:48:08 -0400

    ejercicio nuevo

---
+++ 16.py
@@ -0,0 +1,7 @@
+#-*- coding: utf-8 -*-
+#ejercicio #15
+#Crear un diccionario asociando nombres de países y cantidades de habitantes. 
+# Imprimir luego el diccionario.
+
+Paises={"venezuela": 4, "Colombia": 5, "irlanda":3}
+print Paises
