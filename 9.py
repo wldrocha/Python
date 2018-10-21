9cbc9f2a6019c30278bd661edceb08d62bb970c1
Wladimir Rocha <wld.rocha@gmail.com>
2018-10-21 00:04:53 -0400

    agregado el ejercicio 9

---
+++ 9.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#ejercicio #9
+#Desarrollar una función que reciba tres enteros y nos retorne la suma de los dos más grandes.
+def suma(a,b,c):
+    if a>b and a>c:
+        if b>c:
+            return a+b
+        else:
+            return a+c
+    elif b>a and b>c and c>a:
+        return b+c
+    else:
+        return c+a
+print suma(5,9,7)
+    #x = 5 input('Introduzca el primer digito')
+    #y = 4 input('Introduzca el segundo digito')
+    #z = 3 input('instroduzca el tercer digito')
